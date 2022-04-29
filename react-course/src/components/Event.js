0e36fbd8306c0a5770cd2c52847fcbbf3f5bc011
ehurafa <rafarfl2010@gmail.com>
2022-04-29 19:46:19 -0400

    insert events by  props

--- react-course/src/components/Event.js
+++ react-course/src/components/Event.js
@@ -1,12 +1,19 @@
+import Button from './event/Button'
+
 function Event({number}) {
     function myEvent() {
-        console.log('Opa, fui ativado!', number)
+        console.log('Opa, fui ativado!!!', number)
+    }
+
+    function secondEvent() {
+        console.log('Ativando o segundo evento')
     }
 
     return (
         <div>
             <p>Cliqye para disparar um evento!</p>
-            <button onClick={myEvent}>Ativar!</button>
+            <Button event={myEvent} text="Primeiro Evento" />
+            <Button event={secondEvent} text="Segundo Evento" />
         </div>
     )
 }
