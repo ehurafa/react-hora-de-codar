3509bc6f6f1fe06fd776b076ab804ca4bc077d2a
ehurafa <rafarfl2010@gmail.com>
2022-04-29 19:34:12 -0400

    useState on fire

--- react-course/src/components/Form.js
+++ react-course/src/components/Form.js
@@ -1,14 +1,41 @@
+import { useState } from 'react'
+
 function Form() {
     function sendUser(e) {
         e.preventDefault()
+        console.log(`O usuário ${name} foi cadastrado com a senha ${password}`)
         console.log('Cadastrou o usuário!')
     }
+
+    const [name, setName] = useState('Rafa')
+    const [password, setPassword] = useState()
+
+
+
     return (
         <div>
             <h1>Meu Cadastro</h1>
             <form onSubmit={sendUser}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome" />
+                    <label htmlFor="name">Nome:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Digite o seu nome"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Digite a sua senha"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />
