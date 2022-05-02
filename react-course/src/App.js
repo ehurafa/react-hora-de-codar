b558e5e39594f2182bdb14981bc1534da688ef4e
ehurafa <rafarfl2010@gmail.com>
2022-05-01 21:44:37 -0400

    list rendering

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
-import Conditional from './components/Conditional';
+import OtherList from './components/OtherList'
 
 function App() {
+
+  const list = ['Angular', 'Vue', 'React']
   return (
     <div className="App">
-      <h1>Renderização condicional</h1>
-      <Conditional />
+      <h1>Renderização de listas</h1>
+      <OtherList itens={list} />
     </div>
   );
 }
---
+++ react-course/src/components/OtherList.js
@@ -0,0 +1,18 @@
+function OtherList({ itens }) {
+    return (
+        <>
+            <h3>Lista de coisas boas:</h3>
+            {itens.length > 0 
+                ? (
+                    itens.map((item, index) => {
+                        return <p key={index}>{item}</p>
+                    }))
+                : (
+                        <p>Não há itens na lista</p>
+                    )
+            }
+        </>
+    )
+}
+
+export default OtherList
