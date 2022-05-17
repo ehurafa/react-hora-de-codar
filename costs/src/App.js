47b9928befcaf2b14dbe287423d067f39be54a88
ehurafa <rafarfl2010@gmail.com>
2022-05-16 20:15:03 -0400

    structuring the project

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,9 +1,33 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
+import Container from './layout/Container'
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
-  );
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+          
+            <Route exact path="/" element={<Home/>}/>
+            <Route exact path="/company" element={<Company/>}/>
+            <Route exact path="/contact" element={<Contact/>}/>
+            <Route exact path="/newproject" element={<NewProject/>}/>
+          
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
+  )
 }
 
 export default App;
