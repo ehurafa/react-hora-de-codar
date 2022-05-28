421a122593db9e6b06e2555cefbdcfb810794fc2
ehurafa <rafarfl2010@gmail.com>
2022-05-27 21:01:33 -0400

    system messages

--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -23,7 +23,7 @@ function NewProject() {
             .then((data) => {
                 console.log('data ', data)
                 // redirect
-                navigate('/projects')
+                navigate('/projects', { state: {message: 'Projeto criado com sucesso!'} })
             })
             .catch(err => console.log(err))
     }
