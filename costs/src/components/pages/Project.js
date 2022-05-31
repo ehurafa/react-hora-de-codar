172911ff88d9bfcff6168a57c2b0e65e21eea630
ehurafa <rafarfl2010@gmail.com>
2022-05-31 13:21:58 -0400

    create services area

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -14,6 +14,7 @@ function Project() {
     const { id } = useParams()
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [typeMessage, setTypeMessage] = useState()
 
@@ -33,6 +34,7 @@ function Project() {
     }, [id])
 
     function editPost(project) {
+        setMessage('')
         if(project.budget < project.cost) {
             setMessage('O orçamento não pode ser menor que o custo do projeto!')
             setTypeMessage('error')
@@ -61,6 +63,10 @@ function Project() {
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
     }
 
     return (
@@ -100,6 +106,24 @@ function Project() {
                                     )
                                 }
                             </div>
+                            <div className={styles.service_form_container}>
+                                <h2>Adicione um serviço:</h2>
+                                <button className={styles.btn} onClick={toggleServiceForm}>
+                                    {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+                                </button>
+                                <div className={styles.project_info}>
+                                    { showServiceForm && (
+                                        <div>
+                                            formulário do serviço
+                                        </div>
+                                        )
+                                    }
+                                </div>
+                            </div>
+                            <h2>Serviços</h2>
+                            <Container customClass="start">
+                                <p>Itens de serviços</p>
+                            </Container>
                         </Container>
                     </div>
                 )
